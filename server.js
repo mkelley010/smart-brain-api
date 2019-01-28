c31891db6176feb29276be19d5882bea296514e5
Michael Kelley <mkelley010@gmail.com>
2019-01-28 11:18:00 -0800

    updating database for heroku

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const image = require('./controllers/image');
 const db = knex({
     client: 'pg',
     connection: {
-        host : '127.0.0.1',
+        host : 'postgresql-round-21898',
         user : 'postgres',
         password : '',
         database : 'smartbrain'
